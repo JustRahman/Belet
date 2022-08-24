3e60daf0cd57f5e7456c75a02bf0964cb1671171
JustRahman <rahmanbazarov4567@gmail,com>
2022-08-24 21:44:07 +0500

    first commit

---
+++ ViewModels/LogginPageViewModel.cs
@@ -0,0 +1,127 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Belet.Model;
+using Belet.Views;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Collections.ObjectModel;
+using Belet.Command;
+using Belet.Helper;
+using MaterialDesignThemes.Wpf;
+
+namespace Belet.ViewModels
+{
+    class LogginPageViewModel:ViewModelBase
+    {
+        public DelegateCommand toggleTheme { get; set; }
+
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+
+        bool wndstate = false;
+        private Window _wnd;
+        public Window wnd
+        {
+            get
+            {
+                return _wnd;
+            }
+            set
+            {
+                SetValue(ref _wnd, value);
+            }
+
+        }
+
+        private LogginPasswordModel _logginpasswordmodel;
+        public LogginPasswordModel logginpasswordmodel
+        {
+            get
+            {
+                return _logginpasswordmodel;
+            }
+            set
+            {
+                SetValue(ref _logginpasswordmodel, value);
+            }
+        }
+
+        public MyDelegateCommand InitializeCommand { get; set; }
+        public DelegateCommand Logginbtn { get; set; }
+
+        public MyDelegateCommand close_application { get; set; } 
+        public MyDelegateCommand cmdLogin { get; set; }
+
+
+        public LogginPageViewModel()
+        {
+            logginpasswordmodel = new LogginPasswordModel();
+            InitializeCommand = new MyDelegateCommand(w => InitializeCommand_cmd(w));
+            Logginbtn = new DelegateCommand(()=> Logginbtn_cmd());
+            close_application = new MyDelegateCommand(pep => close_application_cmd(pep)); 
+            cmdLogin = new MyDelegateCommand(zid => cmdLogin_cmd(zid));
+
+            IsDarkTheme = StaticsForTheme.IsBtnChecked;
+            toggleTheme = new DelegateCommand(toggleTheme_cmd);
+
+        }
+
+        private void toggleTheme_cmd()
+        {
+            ITheme theme = paletteHelper.GetTheme();
+
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light); 
+                StaticsForTheme.IsBtnChecked = IsDarkTheme;
+
+            }
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark); 
+                StaticsForTheme.IsBtnChecked = IsDarkTheme;
+
+            }
+            paletteHelper.SetTheme(theme);
+        }
+        private void cmdLogin_cmd(object zid)
+        {
+            wnd.WindowState = WindowState.Minimized;
+        }
+
+
+        
+        
+
+        private void close_application_cmd(object pep)
+        {
+            wnd.Close();
+        }
+
+        private void closeapplication_cmd()
+        {
+            wnd.Close();
+        }
+
+        private void Logginbtn_cmd()
+        {
+            Views.ChoosePage choosePage = new Views.ChoosePage();
+            wnd.Close();
+            choosePage.ShowDialog();
+        }
+
+        private void InitializeCommand_cmd(object o)
+        {
+            object[] wind = o as object[];
+            wnd = (Window)wind[0];
+            //  wnd.Close();
+            //   throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ViewModels/MainChoosePageViewModel.cs
@@ -0,0 +1,57 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Belet.Views;
+using Belet.Model;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Collections.ObjectModel;
+using Belet.Command;
+using Belet.Helper;
+using MaterialDesignThemes.Wpf;
+using System.Diagnostics;
+
+
+
+namespace Belet.ViewModels
+{
+    class MainChoosePageViewModel : ViewModelBase
+    {
+
+
+        public string _myScrollViewer;
+        public string myScrollViewer
+        {
+            get
+            {
+                return _myScrollViewer;
+            }
+            set
+            {
+                SetValue(ref _myScrollViewer , value);
+            }
+        }
+
+        public DelegateCommand btn_click { get; set; }
+
+
+
+        public MainChoosePageViewModel()
+        {
+
+            myScrollViewer = "MyScrollViewer";
+            btn_click = new DelegateCommand(btn_click_cmd);
+
+
+
+
+        }
+
+        private void btn_click_cmd()
+        {
+            
+        }
+    }
+}
